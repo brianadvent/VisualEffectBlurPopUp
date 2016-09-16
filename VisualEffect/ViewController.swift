56f9ceccfbca0be7f87aaa8ac7a9dde0c2be7a05
Training <training@sinatra.fritz.box>
2016-09-16 20:33:24 +0200

    Blurry Pop Up Animation

--- VisualEffect/ViewController.swift
+++ VisualEffect/ViewController.swift
@@ -10,9 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addItemView: UIView!
+    
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    var effect:UIVisualEffect!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        addItemView.layer.cornerRadius = 5
+        
+        
+    }
+    
+    
+    func animateIn() {
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) { 
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+        
+    }
+    
+    
+    func animateOut () {
+        UIView.animate(withDuration: 0.3, animations: { 
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+            
+        }) { (success:Bool) in
+                self.addItemView.removeFromSuperview()
+        }
+    }
+    
+    
+    
+    @IBAction func addItem(_ sender: AnyObject) {
+        
+        animateIn()
+        
+    }
+    @IBAction func dismissPopUp(_ sender: AnyObject) {
+        
+        animateOut()
     }
 
     override func didReceiveMemoryWarning() {
